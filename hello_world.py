dc88508c6038646907b6dd1dfc7ba8f4da1e5a60
puneeth-techie <puneethpgowda.techie@gmail.com>
2020-06-25 17:26:55 +0530

    Initai Commit

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#This is simple basic printing Hello World..!
+print("Hello World..!")
+
+name = "Hello World..!"
+print(name)
+
+first_name = "Hello"
+second_name = "World..!"
+print(first_name + " " + second_name)
